c20a067ab8cd9fe97c6f5394291d36c064c134e2
Giorgi <giorgi.tsintsadze@exxeta.com>
2026-03-15 12:34:19 +0400

    moved received out

--- src/main/java/org/example/SimpleMove.java
+++ src/main/java/org/example/SimpleMove.java
@@ -11,8 +11,8 @@ public class SimpleMove {
     final long TICK_RATE = 60;
     final long TICK_DURATION_MS = 1000 / TICK_RATE; // ≈16 ms per tick
 
-    Set<Client> clients = new HashSet<>();
-    ByteBuffer senderBuffer = ByteBuffer.allocate(4096); // 4 bytes per float * 3
+    public static Set<Client> clients = new HashSet<>();
+    public static ByteBuffer senderBuffer = ByteBuffer.allocate(4096); // 4 bytes per float * 3
 
     public static  boolean deathCubeSpawnMode = false;
     public static float deltaTime = 1f;
@@ -57,51 +57,7 @@ public class SimpleMove {
         }
         System.out.println("Socket Created");
 
-        new Thread(() -> {
-            try {
-                System.out.println("Server listening...");
-
-                byte[] buf = new byte[32];
-                DatagramPacket packet = new DatagramPacket(buf, buf.length);
-                ByteBuffer readerBuffer = ByteBuffer.wrap(buf);
-                while (true) {
-                    socket.receive(packet);
-
-                    readerBuffer.position(0);
-                    readerBuffer.limit(packet.getLength());
-
-                    boolean newClient = true;
-                    Client client = null;
-                    for(Client c : clients) {
-                        if(packet.getAddress().toString().equals(c.ip.toString()) && packet.getPort() == c.port) {
-                            client = c;
-                            newClient = false;
-                        }
-                    }
-                    if(newClient) {
-                        client = new Client(packet.getAddress(), packet.getPort(),senderBuffer);
-                        clients.add(client);
-                        System.out.println("client connected with ip: " + packet.getAddress() + " and port: " + packet.getPort());
-                    }
-
-//                    synchronized(client.inputLock) {
-                        client.latestInput.w = readerBuffer.get() == 1;
-                        client.latestInput.a = readerBuffer.get() == 1;
-                        client.latestInput.s = readerBuffer.get() == 1;
-                        client.latestInput.d = readerBuffer.get() == 1;
-                        client.latestInput.space = readerBuffer.get() == 1;
-                        client.latestInput.leftClick = readerBuffer.get() == 1;
-                        client.latestInput.rightClick = readerBuffer.get() == 1;
-                        client.latestInput.rotationX = readerBuffer.getFloat();
-                        client.latestInput.rotationY = readerBuffer.getFloat();
-                        client.time = readerBuffer.getLong();
-//                    }
-
-                }
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
-        }).start();
+        new Thread(new Receiver(socket)).start();
 
         while(true) {
             long tickStart = System.currentTimeMillis();
